65c6468dcd53d7dfe220a8faeaab34df39117376
AstronauticalCodes <hyperskillabhinav@gmail.com>
2024-12-07 12:25:43 +0530

    STAGE-3-Added Bike details view.

--- Bike Shop/task/shop/urls.py
+++ Bike Shop/task/shop/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.BikeShopView.as_view()),
+    path('<int:pk>/', views.BikeDetailsView.as_view()),
 ]
--- Bike Shop/task/shop/views.py
+++ Bike Shop/task/shop/views.py
@@ -10,3 +10,13 @@ class BikeShopView(View):
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, context={'bikes': self.model.objects.all()})
+
+
+class BikeDetailsView(View):
+    template_name = 'shop/bike_details.html'
+    model = Bike
+
+    def get(self, request, pk, *args, **kwargs):
+        pk = self.kwargs['pk']
+        bike = self.model.objects.filter(id=pk).first()
+        return render(request, self.template_name, context={'bike': bike})
